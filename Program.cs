81d7b8c8e581aea24f34694e0e0af5e9c7101e05
Unixan <115973884+Unixan@users.noreply.github.com>
2023-03-17 14:47:31 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Oppgave_uke_2
+{
+    internal class Program
+    {
+            static List<Student> studentList = new Student(new List<Student>()).StudentList;
+        static void Main()
+        {
+            studentList.Add(new Student("Heibert", 42, "Radiobilkjøring"));
+            studentList.Add(new Student("Susanne", 23, "Radioamatør"));
+            studentList.Add(new Student("Hilde", 45, "Dronepilot"));
+            studentList.Add(new Student("Petter", 19, "Dronepilot"));
+            Menu();
+        }
+        private static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("************ Meny ************\n\n1: Se alle elever\n2: Filtrere elever etter kurs\n3: Avslutt");
+            var choice = Console.ReadLine();
+            MenuSelect(choice);
+        }
+
+        private static void MenuSelect(string choice)
+        {
+            switch (choice)
+            {
+
+
+                case "1":
+                {
+                    listStudents();
+                        break;
+                    }
+                case "2":
+                    {
+                        break;
+                    }
+                case "3":
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        NotAllowed("Invalid choice");
+                        Menu();
+                        break;
+                    }
+            }
+        }
+
+        private static void listStudents()
+        {
+            foreach (var student in studentList)
+            {
+                Console.WriteLine(student.Name);
+            }
+            NotAllowed("");
+            Menu();
+        }
+
+        private static void NotAllowed(string msg)
+        {
+            Console.WriteLine(msg + "\nTrykk en tast for å fortsette . . .");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,24 @@
+﻿namespace Oppgave_uke_2
+{
+    internal class Student
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string Course { get; private set; }
+        public int Id { get; private set; }
+        public List<Student> StudentList { get; set; }
+
+
+        public Student(string name, int age, string course)
+        {
+            Name = name;
+            Age = age;
+            Course = course;
+        }
+
+        public Student(List<Student> studentList)
+        {
+            StudentList = studentList;
+        }
+    }
+}
